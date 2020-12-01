47b2e74fe2b8fdc24a8da033ab32f93a56e1ade0
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-01 14:51:12 +0100

    solved day1 part2
    also modified d1 p1 code

--- 2020/day1/1.js
+++ 2020/day1/1.js
@@ -1,12 +1,26 @@
 const fs = require('fs');
-const array = fs.readFileSync('./in1.txt').toString().split('\r\n').map(x => parseInt(x));
+const in1 = fs.readFileSync('./in1.txt').toString().split('\r\n').map(x => parseInt(x));
 
-function GetOutput() {
-    for (var i = 0; i < array.length; i++) {
-        for (var j = 0; j < array.length; j++) {
-            if (array[i] + array[j] === 2020) return array[i] * array[j];
+function P1Out(input) {
+    for (var i = 0; i < input.length; i++) {
+        for (var j = 0; j < input.length; j++) {
+            if (input[i] + input[j] === 2020) return input[i] * input[j];
         }
     }
 }
 
-console.log(GetOutput());
+console.log(P1Out(in1));
+
+const in2 = fs.readFileSync('./in2.txt').toString().split('\r\n').map(x => parseInt(x));
+
+function P2Out(input) {
+    for (var i = 0; i < input.length; i++) {
+        for (var j = 0; j < input.length; j++) {
+            for (var k = 0; k < input.length; k++) {
+                if (input[i] + input[j] + input[k] === 2020) return input[i] * input[j] * input[k];
+            }
+        }
+    }
+}
+
+console.log(P2Out(in2));
