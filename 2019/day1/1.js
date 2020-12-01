0d94d554b7afd87d86cf0e064d2041c912b18338
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-01 20:55:34 +0100

    part two complete

--- 2019/day1/1.js
+++ 2019/day1/1.js
@@ -11,3 +11,26 @@ function P1() {
 }
 
 console.log(P1());
+fs.writeFileSync('output.txt', `Part one: ${P1()}`);
+
+// part two
+function P2() {
+    let output = 0;
+
+    function Calculate(item) {
+        let result = Math.floor(item / 3) - 2;
+        output += result;
+        if (Math.floor(result / 3) - 2 > 0) {
+            Calculate(result);
+        }
+    }
+
+    input.forEach((item, i) => {
+        Calculate(item);
+    });
+
+    return output;
+}
+
+console.log(P2());
+fs.appendFileSync('output.txt', `\nPart two: ${P2()}`);
