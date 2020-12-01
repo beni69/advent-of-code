d95f5f9a2778f4d67334b05be935ee8c96a4fac5
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-01 21:10:14 +0100

    setup
    ps: might make a script for this

--- 2020/day2/2.js
+++ 2020/day2/2.js
@@ -2,3 +2,16 @@ const fs = require('fs');
 const input = fs.readFileSync('./in.txt').toString().split('\r\n').map(x => parseInt(x)).filter(x => !isNaN(x));
 
 console.log(input);
+
+// part one
+function P1() {
+
+}
+
+// part two
+function P2() {
+
+}
+
+fs.writeFileSync('output.txt', `Part one: ${P1()}`);
+fs.appendFileSync('output.txt', `\nPart two: ${P2()}`);
