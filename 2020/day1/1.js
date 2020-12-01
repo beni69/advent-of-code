40538ad2126018254352606de8b08fe0ed75ec8b
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-01 20:35:25 +0100

    code now ignores nan elements
    should be more robust

--- 2020/day1/1.js
+++ 2020/day1/1.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 // part one
 function P1() {
-    const input = fs.readFileSync('./in.txt').toString().split('\r\n').map(x => parseInt(x));
+    const input = fs.readFileSync('./in.txt').toString().split('\r\n').map(x => parseInt(x)).filter(x => !isNaN(x));
     for (var i = 0; i < input.length; i++) {
         for (var j = 0; j < input.length; j++) {
             if (input[i] + input[j] === 2020) return input[i] * input[j];
@@ -15,7 +15,7 @@ fs.writeFileSync('output.txt', `Part one: ${P1()}`);
 
 // part two
 function P2() {
-    const input = fs.readFileSync('./in.txt').toString().split('\r\n').map(x => parseInt(x));
+    const input = fs.readFileSync('./in.txt').toString().split('\r\n').map(x => parseInt(x)).filter(x => !isNaN(x));
 
     for (var i = 0; i < input.length; i++) {
         for (var j = 0; j < input.length; j++) {
