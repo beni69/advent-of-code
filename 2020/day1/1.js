6f2766f8416e535c3a09cb07281ba1737d2b8e57
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-01 15:18:31 +0100

    renamed kinda everything
    output now goes to a file

--- 2020/day1/1.js
+++ 2020/day1/1.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-const in1 = fs.readFileSync('./in1.txt').toString().split('\r\n').map(x => parseInt(x));
 
-function P1Out(input) {
+// part one
+function P1() {
+    const input = fs.readFileSync('./in1.txt').toString().split('\r\n').map(x => parseInt(x));
     for (var i = 0; i < input.length; i++) {
         for (var j = 0; j < input.length; j++) {
             if (input[i] + input[j] === 2020) return input[i] * input[j];
@@ -9,11 +10,13 @@ function P1Out(input) {
     }
 }
 
-console.log(P1Out(in1));
+console.log(P1());
+fs.writeFileSync('output.txt', `Part one: ${P1()}`)
 
-const in2 = fs.readFileSync('./in2.txt').toString().split('\r\n').map(x => parseInt(x));
+// part two
+function P2() {
+    const input = fs.readFileSync('./in2.txt').toString().split('\r\n').map(x => parseInt(x));
 
-function P2Out(input) {
     for (var i = 0; i < input.length; i++) {
         for (var j = 0; j < input.length; j++) {
             for (var k = 0; k < input.length; k++) {
@@ -23,4 +26,5 @@ function P2Out(input) {
     }
 }
 
-console.log(P2Out(in2));
+console.log(P2());
+fs.appendFileSync('output.txt', `\nPart two: ${P2()}`);
