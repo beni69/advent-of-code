2c0121a55d403016193757b999ad87ed235091e6
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-04 13:39:11 +0100

    simplicication

--- 2020/day4/4.js
+++ 2020/day4/4.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const input = fs.readFileSync('./in.txt').toString().trim().split('\n\n');
-// console.log(input);
 
 function P1() {
     let valid = 0;
@@ -24,7 +23,6 @@ function P2() {
         let pass = {};
         for (var j = 0; j < item.length; j++) {
             if (item[j].startsWith('hgt:')) {
-                // console.log('hgt detected');
 
                 const value = parseInt(item[j].substring(4, item[j].length - 2));
                 if (item[j].endsWith('in') && value >= 59 && value <= 76) {
@@ -35,42 +33,36 @@ function P2() {
 
                 }
             } else if (item[j].startsWith('byr:')) {
-                // console.log('byr detected');
                 const value = parseInt(item[j].substring(4));
                 if (value >= 1920 && value <= 2002) {
                     pass.byr = true;
                 }
 
             } else if (item[j].startsWith('iyr:')) {
-                // console.log('iyr detected');
                 const value = parseInt(item[j].substring(4));
                 if (value >= 2010 && value <= 2020) {
                     pass.iyr = true;
                 }
 
             } else if (item[j].startsWith('eyr:')) {
-                // console.log('eyr detected');
                 const value = parseInt(item[j].substring(4));
                 if (value >= 2020 && value <= 2030) {
                     pass.eyr = true;
                 }
 
             } else if (item[j].startsWith('hcl:')) {
-                // console.log('hcl detected');
                 const value = item[j].substring(4);
                 if (/^#[0-9A-F]{6}$/i.test(value)) {
                     pass.hcl = true;
                 }
 
             } else if (item[j].startsWith('ecl:')) {
-                // console.log('ecl detected');
                 const value = item[j].substring(4);
                 if (/^amb|blu|brn|gry|grn|hzl|oth$/i.test(value)) {
                     pass.ecl = true;
                 }
 
             } else if (item[j].startsWith('pid:')) {
-                // console.log('pid detected');
                 const value = item[j].substring(4);
                 if (!isNaN(parseInt(value)) && value.toString().length == 9) {
                     pass.pid = true;
@@ -93,25 +85,6 @@ function P2() {
     });
     return valid;
 }
-
-function Test(str) {
-
-    // console.log(str.substring(4, str.length - 2));
-    // if (str.endsWith('in') && parseInt(str.substring(4, str.length - 2)) >= 59 && parseInt(str.substring(4, str.length - 2)) <= 76) {
-    //     console.log('inch correct!');
-    // } else if (str.endsWith('cm') && parseInt(str.substring(4, str.length - 2)) >= 150 && parseInt(str.substring(4, str.length - 2)) <= 193) {
-    //     console.log('cm correct!');
-    // }
-    // console.log(/^#[0-9A-F]{6}$/i.test(str));
-    // const value = parseInt(str.substring(4));
-    // console.log(!isNaN(parseInt(str)) && str.toString().length == 9);
-
-}
-
-// console.log(input);
-// console.log(P1());
-// console.log(P2());
-// Test('pid:087499704');
 
 fs.writeFileSync('output.txt', `Part one: ${P1()}`);
 fs.appendFileSync('output.txt', `\nPart two: ${P2()}`);
