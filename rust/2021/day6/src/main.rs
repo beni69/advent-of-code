759cad8161e8865a214f859b784879ca9d8bca2a
beni69 <73666440+beni69@users.noreply.github.com>
2021-12-06 09:22:19 +0100

    day 6 done

--- rust/2021/day6/src/main.rs
+++ rust/2021/day6/src/main.rs
@@ -4,15 +4,14 @@ type Fish = [u128; 9];
 
 fn main() {
     let input_string = get_input_string();
-    // println!("{:?}", &inp);
     let mut input = Vec::new();
     for num in input_string.split(",") {
         input.push(num.parse::<u128>().unwrap());
     }
     let input: Fish = transform(input);
-    println!("{:?}", &input);
-    println!("part 1: {}", part1(&input));
-    println!("part 2: {}", part2(&input));
+    println!("input: {:?}", &input);
+    println!("part 1: {}", simulate_n_days(&input, 80));
+    println!("part 2: {}", simulate_n_days(&input, 256));
 }
 
 fn transform(nums: Vec<u128>) -> Fish {
@@ -24,53 +23,23 @@ fn transform(nums: Vec<u128>) -> Fish {
 }
 
 fn simulate_day(fish: &mut Fish) {
-    let tmp_fish = fish.clone();
-    // let len = fish.len();
-    // for i in 0..len {
-    //     if fish[i] > 0 {
-    //         fish[i] -= 1;
-    //     } else {
-    //         fish[i] = 6;
-    //         // fish.push(8);
-    //     }
-    // }
-    // for i in 0..fish.len() - 1 {
-    // if i == 0 {
-    //     fish[6] = fish[0];
-    //     fish[8] = fish[0];
-    //     fish[0] = 0;
-    // }
-    // }
-    for i in (0..fish.len() - 1).rev() {
-        dbg!(&i);
-        if i == 6 || i == 8 {
-            fish[i] = tmp_fish[0];
-        } else {
-            fish[i] = tmp_fish[i + 1];
-        }
+    let new=fish[0];
+    for i in 0..fish.len() - 1 {
+        fish[i] = fish[i + 1];
     }
-    // fish[0] = 1000000;
+    fish[8]=new;
+    fish[6]+=new;
 }
 
 fn count_fish(fish: &Fish) -> u128 {
     fish.iter().sum()
 }
 
-fn part1(nums: &Fish) -> u128 {
+fn simulate_n_days(nums: &Fish, n: i32) -> u128 {
     let mut fish = nums.clone();
-    for _ in 0..18 {
+    for _ in 0..n {
         simulate_day(&mut fish);
-        dbg!(&fish);
     }
-
     count_fish(&fish)
 }
 
-fn part2(nums: &Fish) -> usize {
-    let mut fish = nums.clone();
-    // for _i in 0..256 {
-    //     dbg!(_i);
-    //     simulate_day(&mut fish);
-    // }
-    fish.len()
-}
