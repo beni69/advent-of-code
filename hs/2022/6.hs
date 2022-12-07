c643c4b3906e7428038fbaf336f8da8d803a4364
beni69 <73666440+beni69@users.noreply.github.com>
2022-12-07 10:39:33 +0100

    day7 done

---
+++ hs/2022/6.hs
@@ -0,0 +1,4 @@
+main :: IO ()
+main = do
+  input <- readFile "../js-ts/.cache/input-2022-6.txt"
+  print input
--- rust/2022/day7/src/main.rs
+++ rust/2022/day7/src/main.rs
@@ -1,10 +1,11 @@
-use indextree::{Arena, NodeId};
+use indextree::Arena;
 use lib::{input::get_input_lines, year_day, Result};
 
 fn main() -> Result<()> {
     let input = get_input_lines(year_day!())?;
     let mut tree = Arena::new();
-    let mut cwd = tree.new_node(("/", 0u32));
+    let root = tree.new_node(("/", 0u32));
+    let mut cwd = root;
 
     for line in input.iter().skip(1) {
         let mut s = line.split_ascii_whitespace();
@@ -13,14 +14,10 @@ fn main() -> Result<()> {
             "$" => match s.next().unwrap() {
                 "cd" => {
                     let dir = s.next().unwrap();
-                    // if dir == "/" {
-                    //     cwd = find_root(&tree);
-                    //     continue;
-                    // }
                     if dir == ".." {
-                        let next = cwd.ancestors(&tree).nth(1).unwrap();
-                        tree.get_mut(next).unwrap().get_mut().1 += tree.get(cwd).unwrap().get().1;
-                        cwd = next;
+                        let up = cwd.ancestors(&tree).nth(1).unwrap();
+                        tree.get_mut(up).unwrap().get_mut().1 += tree.get(cwd).unwrap().get().1;
+                        cwd = up;
                         continue;
                     }
                     let n = tree.new_node((dir, 0));
@@ -34,40 +31,31 @@ fn main() -> Result<()> {
             _ => {
                 let x: u32 = c.parse().unwrap();
                 tree.get_mut(cwd).unwrap().get_mut().1 += x;
-            } // _ => sum += c.parse::<u32>().unwrap(),
+            }
         }
     }
-    let next = cwd.ancestors(&tree).nth(1).unwrap();
-    tree.get_mut(next).unwrap().get_mut().1 += tree.get(cwd).unwrap().get().1;
+    // add remaining files all the way up
+    while let Some(up) = cwd.ancestors(&tree).nth(1) {
+        tree.get_mut(up).unwrap().get_mut().1 += tree.get(cwd).unwrap().get().1;
+        cwd = up;
+    }
 
-    let root = find_root(&tree);
-
-    println!("{:?}", root.debug_pretty_print(&tree));
+    // eprintln!("{:?}", root.debug_pretty_print(&tree));
 
     let sum = tree
         .iter()
         .map(|n| if n.get().1 <= 100_000 { n.get().1 } else { 0 })
         .sum::<u32>();
-    dbg!(sum);
+    println!("part 1: {sum}");
 
     let used = tree.get(root).unwrap().get().1;
-    dbg!(used);
-    let needed = (70_000_000 - tree.get(root).unwrap().get().1);
-    dbg!(needed);
+    assert!(used >= 40_000_000, "not all files were counted");
+    let needed = 30_000_000 - (70_000_000 - tree.get(root).unwrap().get().1);
 
     let mut d = tree.iter().map(|n| n.get().1).collect::<Vec<_>>();
     d.sort_unstable();
-    let res = d.iter().find(|x| **x >= needed).unwrap();
-    dbg!(res);
+    let res = d.into_iter().find(|x| *x >= needed).unwrap();
+    println!("part 2: {res}");
 
     Ok(())
-}
-
-fn find_root(tree: &Arena<(&str, u32)>) -> NodeId {
-    tree.get_node_id(
-        tree.iter()
-            .find(|n| !n.is_removed() && n.get().0 == "/")
-            .unwrap(),
-    )
-    .unwrap()
 }
