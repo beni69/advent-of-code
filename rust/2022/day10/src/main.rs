fd81cd6e55d691a9b92b7fd153ab3e28382ac622
beni69 <73666440+beni69@users.noreply.github.com>
2022-12-10 15:37:32 +0100

    tiny code improvements

--- rust/2022/day10/src/main.rs
+++ rust/2022/day10/src/main.rs
@@ -7,8 +7,8 @@ fn main() -> Result<()> {
     let mut x = 1;
     let mut done = -1;
     let mut cmd = Vec::new();
-    let mut res = Vec::new();
-    let mut screen: Vec<char> = vec!['.'; 240];
+    let mut res = 0;
+    let mut screen: Vec<bool> = vec![false; 240];
 
     for i in 1..241 {
         // before cycle
@@ -28,38 +28,32 @@ fn main() -> Result<()> {
 
         // during cycle
         if (i - 20) % 40 == 0 {
-            res.push(x * i);
+            res += x * i;
             // eprintln!("{i}: {x} - {}", x * i);
         }
         // part 2
         let y = (i - 1) % 40;
         if x >= y - 1 && x <= y + 1 {
-            screen[(i - 1) as usize] = '#';
+            screen[(i - 1) as usize] = true;
         }
 
         // after cycle
         if done == i {
             if cmd[0] == "addx" {
-                x += cmd[1].parse::<i32>().unwrap();
+                x += cmd[1].parse::<i32>().unwrap()
             }
-            done = -1;
+            done = -1
         }
     }
 
-    println!("part 1: {}", res.iter().sum::<i32>());
-
-    println!("part 2:\n{}", render(&screen).trim());
+    println!("part 1: {res}");
+    println!("part 2:");
+    for (i, b) in screen.iter().enumerate() {
+        print!("{}", if *b { "#" } else { "." });
+        if (i + 1) % 40 == 0 {
+            println!()
+        }
+    }
 
     Ok(())
-}
-fn render(screen: &Vec<char>) -> String {
-    let mut s = String::with_capacity(246);
-    for (i, c) in screen.iter().enumerate() {
-        s.push(*c);
-        if (i + 1) % 40 == 0 {
-            s.push('\n');
-        }
-    }
-    assert_eq!(s.len(), 246);
-    s
 }
