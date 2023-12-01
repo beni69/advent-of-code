a97382e9408211229aff4f598edd1d494532a7cd
beni69 <73666440+beni69@users.noreply.github.com>
2023-12-01 09:50:33 +0100

    day1p2 done

--- rust/2023/day1/src/main.rs
+++ rust/2023/day1/src/main.rs
@@ -1,54 +1,50 @@
 use std::error::Error;
-
 const DIGITS: [&str; 9] = [
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
-
+fn check(line: &str, i: usize) -> Option<u32> {
+    let c = line.chars().skip(i).next().unwrap();
+    if c.is_numeric() {
+        return c.to_digit(10);
+    }
+    for (j, d) in DIGITS.iter().enumerate() {
+        if d.len() + i > line.len() {
+            continue;
+        }
+        if **d == line[i..i + d.len()] {
+            return Some(j as u32 + 1);
+        }
+    }
+    None
+}
 fn main() -> Result<(), Box<dyn Error>> {
-    let file = std::fs::read_to_string("./test")?;
-    let inp: Vec<_> = file.trim().split("\n").collect();
-    // println!("{inp:#?}");
+    let file = std::fs::read_to_string("./in")?;
+    let inp = file.trim().split("\n");
 
     let mut sol = 0u32;
     for line in inp {
-        let mut i = 0usize;
-        let mut c1: Option<char> = None;
-        let mut c2: Option<char> = None;
-        while i < line.len() {
-            let c = line.chars().skip(i).next().unwrap();
-
-            if c.is_numeric() {
-                c2 = Some(c);
-                if c1.is_none() {
-                    c1 = c2.clone();
-                }
-            } else {
-                for (j, d) in DIGITS.iter().enumerate() {
-                    if d.len() + i > line.len() {
-                        continue;
-                    }
-                    // dbg!(&line[i..i + d.len()], d);
-                    if **d == line[i..i + d.len()] {
-                        dbg!(d);
-                        c2 = Some((j + 1).to_string().chars().next().unwrap());
-                        if c1.is_none() {
-                            c1 = c2;
-                        }
-                        i += d.len() - 1;
-                    }
-                }
-            }
-
+        let mut i = 0;
+        let mut c1: Option<u32> = None;
+        while i < line.len() && c1.is_none() {
+            c1 = check(line, i);
             i += 1;
         }
-        dbg!(&c1, &c2);
-        match (c1, c2) {
-            (Some(c1), Some(c2)) => sol += format!("{c1}{c2}").parse::<u32>().unwrap(),
-            _ => unreachable!(),
+        i = line.len() - 1;
+        let mut c2: Option<u32> = None;
+        while c2.is_none() {
+            c2 = check(line, i);
+            if i == 0 {
+                break;
+            } else {
+                i -= 1;
+            }
+        }
+        if let (Some(c1), Some(c2)) = (c1, c2) {
+            sol += c1 * 10 + c2;
+        } else {
+            unreachable!()
         }
     }
-
     println!("{sol}");
-
     Ok(())
 }
