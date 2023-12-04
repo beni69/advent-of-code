556caadf2ef2b95dfac762a34ba44c440c42a40c
beni69 <73666440+beni69@users.noreply.github.com>
2023-12-04 11:05:43 +0100

    day4p2 done

--- rust/2023/day4/src/main.rs
+++ rust/2023/day4/src/main.rs
@@ -3,41 +3,30 @@ use lib::{input::get_input, year_day, Result};
 
 fn main() -> Result<()> {
     let inp = get_input(year_day!())?;
-
-    let mut sum = 0;
-    for line in inp.trim().lines() {
-        let x = line[line.find(':').unwrap() + 2..]
-            .split('|')
-            .flat_map(|p| {
-                p.split_ascii_whitespace()
-                    .map(|x| x.parse::<u32>().unwrap())
-            });
-
-        let winners = x.duplicates().count();
-        if winners > 0 {
-            sum += 2u32.pow(winners as u32 - 1);
-        }
-    }
-    println!("{sum}");
-
-    let mut s2 = 0;
-    let lines = inp
+    let cards = inp
         .trim()
         .lines()
         .map(|l| {
-            l[l.find(':').unwrap() + 2..]
-                .split('|')
-                .map(|p| {
-                    p.split_ascii_whitespace()
-                        .map(|x| x.parse::<u32>().unwrap())
-                        .collect_vec()
-                })
-                .collect_tuple::<(_, _)>()
-                .unwrap()
+            l[l.find(':').unwrap() + 2..].split('|').map(|p| {
+                p.split_ascii_whitespace()
+                    .map(|x| x.parse::<u32>().unwrap())
+            })
         })
         .collect_vec();
 
-    eprintln!("{lines:?}");
-
-    Ok(())
+    let mut s1 = 0;
+    let mut count = vec![1; cards.len()];
+    for (i, card) in cards.into_iter().enumerate() {
+        let winners = card.flatten().duplicates().count();
+        // part 1
+        if winners > 0 {
+            s1 += 2u32.pow(winners as u32 - 1);
+        }
+        // part 2
+        for j in i + 1..i + 1 + winners {
+            count[j] += count[i];
+        }
+    }
+    let s2: u32 = count.iter().sum();
+    Ok(println!("{s1}\n{s2}"))
 }
