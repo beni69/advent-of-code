b5df381d3bea2aac3ba0c6425f26a2cc19f2a67f
beni69 <73666440+beni69@users.noreply.github.com>
2023-12-05 22:16:58 +0100

    d5p2 wip
    works on the test input but not the real one :((

--- rust/2023/day5/src/main.rs
+++ rust/2023/day5/src/main.rs
@@ -9,6 +9,9 @@ fn main() -> Result<()> {
         .map(|x| x.parse::<i64>().unwrap())
         .collect_vec();
 
+    let s2 = seeds.clone();
+    let l2 = lines.clone();
+
     for map in lines {
         // prepare map
         let transforms = map
@@ -34,5 +37,79 @@ fn main() -> Result<()> {
         }
     }
     println!("{}", seeds.iter().min().unwrap());
+
+    let mut seeds = s2
+        .into_iter()
+        .tuples::<(_, _)>()
+        // (start, len) -> (start, end)
+        .map(|(start, len)| (start, start + len - 1))
+        .collect_vec();
+    for map in l2 {
+        for s in &seeds {
+            eprintln!("{s:?}");
+        }
+        let transforms = map
+            .lines()
+            .skip(1)
+            .map(|l| {
+                let nums = l
+                    .split_ascii_whitespace()
+                    .map(|x| x.parse::<i64>().unwrap())
+                    .collect_vec();
+                // (start, end, x)
+                (nums[1], nums[1] + nums[2], nums[0] - nums[1])
+            })
+            .collect_vec();
+
+        let s2 = seeds.clone();
+        let mut rm = Vec::new(); // prevent index shift
+        dbg!(s2.len());
+        for i in 0..s2.len() {
+            for tr in &transforms {
+                let start = i64::max(s2[i].0, tr.0);
+                let end = i64::min(s2[i].1, tr.1);
+
+                // no overlap
+                if end < start {
+                    continue;
+                }
+
+                rm.push(i);
+                seeds.push((start + tr.2, end + tr.2));
+                if s2[i].0 < start {
+                    seeds.push((s2[i].0, start - 1));
+                }
+                if s2[i].1 > end {
+                    seeds.push((end + 1, s2[i].1));
+                }
+
+                eprintln!("\n===== {tr:?}");
+                for s in &seeds {
+                    eprintln!("{s:?}");
+                }
+                eprintln!("=====\n");
+
+                break;
+            }
+        }
+        eprintln!("rm: {rm:?}");
+        rm.into_iter().rev().for_each(|i| {
+            seeds.remove(i);
+        });
+        dbg!(seeds.len());
+    }
+
+    for s in &seeds {
+        eprintln!("{s:?}");
+    }
+
+    let min = seeds
+        .iter()
+        .map(|(start, _)| start)
+        .filter(|x| **x != 0)
+        .min()
+        .unwrap();
+    println!("{min}");
+
     Ok(())
 }
